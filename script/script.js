5f571416a70e15836aa8623b0b3b2461c1592259
Paul Jimuel Catalan <jimuelcatalan@gmail.com>
2019-11-30 23:25:07 +0800

    Improved functionality; Visual Improvements

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 var currentX, currentY;
 var prevX, prevY;
+var defaultPointerLocation = '69 69';
 
 function userClicked() {
     var x = event.clientX;
@@ -8,11 +9,10 @@ function userClicked() {
     currentX = x;
     currentY = y;
 
-    var snowball = document.getElementById("circle");
-    document.getElementById("coordinates").innerHTML = "x=" + x + "<br> y=" + y;
-    snowball.style.position = "absolute";
-    snowball.style.left = (x - 5) + 'px';
-    snowball.style.top = (y - 5) + 'px';
+    var coordinates = x + ' ' + y;
+    movePointer(coordinates);
+    addPoint();
+    document.getElementById('coordinates').innerHTML = 'x=' + x + '<br> y=' + y;
 }
 
 function addPoint() {
@@ -23,32 +23,58 @@ function addPoint() {
     prevX = currentX;
     prevY = currentY;
 
-    var points = document.getElementById("points").value;
+    var points = document.getElementById('points').value;
     if (points.length > 0) {
-        document.getElementById("points").value += ", " + currentX + " " + currentY;
+        document.getElementById('points').value += ', ' + currentX + ' ' + currentY;
+    } else {
+        document.getElementById('points').value += currentX + ' ' + currentY;
     }
-    else {
-        document.getElementById("points").value += currentX + " " + currentY;
-    }
+    showPath();
 }
 
-function clearForm() {
-    document.getElementById("points").value = "";
-    document.getElementsByTagName("svg")[0].innerHTML = "";
+function movePointer(coordinates) {
+    if (undefined == coordinates) {
+        coordinates = defaultPointerLocation;
+    }
+
+    coordinates = coordinates.trim().split(' ');
+    var snowball = document.getElementById('circle');
+    snowball.style.position = 'absolute';
+    snowball.style.left = coordinates[0] - 5 + 'px';
+    snowball.style.top = coordinates[1] - 5 + 'px';
+}
+
+function clearPoint() {
+    var newPointerLocation = '';
+    var points = document.getElementById('points');
+    var newPoints = points.value.split(',');
+    if (newPoints.length > 0) {
+        newPoints.pop();
+        newPointerLocation = newPoints[newPoints.length - 1];
+        newPoints.join(', ');
+    }
+    points.value = newPoints;
+    showPath();
+    movePointer(newPointerLocation);
+}
+
+function resetPoints() {
+    document.getElementById('points').value = '';
+    document.getElementsByTagName('svg')[0].innerHTML = '';
+    movePointer(defaultPointerLocation);
 }
 
 function parseSVG(s) {
     var div = document.createElementNS('http://www.w3.org/1999/xhtml', 'div');
     div.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg">' + s + '</svg>';
     var frag = document.createDocumentFragment();
-    while (div.firstChild.firstChild)
-        frag.appendChild(div.firstChild.firstChild);
+    while (div.firstChild.firstChild) frag.appendChild(div.firstChild.firstChild);
     return frag;
 }
 
-function show() {
-    document.getElementsByTagName("svg")[0].innerHTML = "";
-    var points = document.getElementById("points").value;
+function showPath() {
+    document.getElementsByTagName('svg')[0].innerHTML = '';
+    var points = document.getElementById('points').value;
     var e = "<polyline  fill='none' stroke='red'  stroke-width='3' points='" + points + "'></polyline>";
-    document.getElementsByTagName("svg")[0].appendChild(parseSVG(e));
+    document.getElementsByTagName('svg')[0].appendChild(parseSVG(e));
 }
